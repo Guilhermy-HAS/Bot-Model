014dade84e0126fcd4b2d8edcb2d007fec72d08e
Guilhermy-HAS <77300993+Guilhermy-HAS@users.noreply.github.com>
2021-01-13 22:01:49 -0300

    Create addrole.js
    Um comando de addrole para adicionar cargos a outros usuarios, bem fácil de fazer e que pode ser bem variavel.

---
+++ addrole.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+  
+const perms = message.member.permissions.has("MANAGE_ROLES")
+if(!perms) return message.reply("Você nao tem permissão.")
+  
+  let role = message.mentions.roles.first()
+  if(!role) return message.reply("❌ Este cargo nao existe.")
+  let member = message.mentions.members.first()
+  if(!member) return message.reply("❌ Mencione alguem!")
+ try {
+  member.roles.add(role.id)
+  message.channel.send("✅ Cargo adicionado com sucesso!")
+  
+}catch(err) {
+ console.error(err)
+}
+}
